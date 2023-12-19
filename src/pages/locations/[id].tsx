f4ab9d68202345a2fdd005740a04dd32603bcc16
Enes Ayduran <ayduranenes@gmail.com>
2023-12-19 19:39:09 +0300

    feat(): fetch data by id

--- src/pages/locations/[id].tsx
+++ src/pages/locations/[id].tsx
@@ -1,11 +1,7 @@
-import { useEffect, useState } from "react";
 import LocationCard from "../../components/LocationCard";
 import Image from "next/image";
 import Pagination from "@/components/Pagination";
-// import Pagination from "../../components/Pagination";
-// import { useLocation, useNavigate } from "react-router-dom";
-// import "./index.css";
-// import Skeleton from "react-loading-skeleton";
+import { useRouter } from "next/router";
 
 export interface LocationSpec {
   id: number;
@@ -16,24 +12,13 @@ export interface LocationSpec {
 }
 
 function Location({ data }: { data: any }) {
-  const [locations, setLocations] = useState<never[]>([]); // Provide a type for the characters state variable
-  const [totalPages, setTotalPages] = useState<number>(50);
-  // const route = useLocation();
-  // const pageNumber = route.pathname.split("/locations/")[1];
-  // const navigate = useNavigate();
+  const router = useRouter();
+  const { id } = router.query;
 
   function GoToPage(page: number) {
-    // navigate(`/locations/${page}`);
+    router.push(`/locations/${page}`);
   }
 
-  // useEffect(() => {
-  //   fetch(`https://rickandmortyapi.com/api/location/?page=${1}`)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setTotalPages(data.info.pages);
-  //       setLocations(data.results);
-  //     });
-  // }, []);
   return (
     <div className="flex flex-col justify-between items-center bg-white h-screen">
       <div className="flex flex-col items-center justify-center gap-y-2">
@@ -43,30 +28,26 @@ function Location({ data }: { data: any }) {
             alt="Rick and Morty Banner"
             width={200}
             height={200}
+            priority
           />
         </div>
         <div className="mx-16">
           <div className="w-full font-bold text-3xl mb-4">Locations</div>
           {data.results ? (
             <ul className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-y-2 gap-x-4">
-              {data.results.map(
-                (
-                  location: LocationSpec // Remove the type annotation for character
-                ) => (
-                  <LocationCard key={location.id} location={location} />
-                )
-              )}
+              {data.results.map((location: LocationSpec) => (
+                <LocationCard key={location.id} location={location} />
+              ))}
             </ul>
           ) : (
-            // <Skeleton count={10} />
             <div>Loading...</div>
           )}
         </div>
       </div>
       <div className="my-4 pb-4">
         <Pagination
-          totalPages={totalPages}
-          currentPage={parseInt("2")}
+          totalPages={data.info.pages}
+          currentPage={parseInt(id as string)}
           onPageChange={GoToPage}
         />
       </div>
@@ -76,11 +57,16 @@ function Location({ data }: { data: any }) {
 
 export default Location;
 
-export async function getServerSideProps() {
+import { GetServerSidePropsContext } from "next";
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { req } = context;
+  const { url } = req;
+
   // Fetch data
   try {
     const response = await fetch(
-      `https://rickandmortyapi.com/api/location/?page=${1}`
+      `https://rickandmortyapi.com/api/location/?page=${url?.split("?id=")[1]}`
     );
     const data = await response.json();
 
