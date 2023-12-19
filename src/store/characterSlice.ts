8286470a7f40a12b31dc5f2c875e522b94098c34
Enes Ayduran <ayduranenes@gmail.com>
2023-12-19 19:40:42 +0300

    feat(): add store for global states

---
+++ src/store/characterSlice.ts
@@ -0,0 +1,37 @@
+// characterSlice.ts
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "./store";
+import { CharacterSpec } from "../pages/Characters";
+
+export interface Character {
+  id: number;
+  name: string;
+}
+
+interface CharacterState {
+  characters: CharacterSpec[];
+}
+
+const initialState: CharacterState = {
+  characters: [],
+};
+
+const characterSlice = createSlice({
+  name: "characters",
+  initialState,
+  reducers: {
+    addCharacter(state, action: PayloadAction<CharacterSpec>) {
+      state.characters.push(action.payload);
+    },
+    deleteCharacter(state, action: PayloadAction<number>) {
+      state.characters = state.characters.filter(
+        (char) => char.id !== action.payload
+      );
+    },
+  },
+});
+
+export const { addCharacter, deleteCharacter } = characterSlice.actions;
+export const selectCharacters = (state: RootState) =>
+  state.characters.characters;
+export default characterSlice.reducer;
---
+++ src/store/store.ts
@@ -0,0 +1,14 @@
+// store.ts
+import { configureStore } from "@reduxjs/toolkit";
+import characterReducer from "./characterSlice";
+
+const store = configureStore({
+  reducer: {
+    characters: characterReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
